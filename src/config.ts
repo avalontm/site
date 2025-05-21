2b9b9f924f906fb21962f50b0a6965c00712da35
AvalonTM <AvalonTM@DESKTOP-2J67J2O>
2025-05-21 11:51:29 -0700

    actualizacion del 21-05-2025

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 const config = {
   title: 'long play record store',
   descripcion: 'Tu tienda en línea con los mejores productos y ofertas.',
-  apiBase: 'https://avalontm.info', // Asegúrate de que la URL base esté bien configurada
-  apiUrl: 'https://avalontm.info/api', // Asegúrate de que la URL base esté bien configurada
-  socketUrl: 'wss://avalontm.info',
+  apiBase: 'http://192.168.100.7:8081', // Asegúrate de que la URL base esté bien configurada
+  apiUrl: 'http://192.168.100.7:8081/api', // Asegúrate de que la URL base esté bien configurada
+  socketUrl: 'wss://192.168.100.7',
 };
 export default config;
