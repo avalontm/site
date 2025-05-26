5b9150cc4bae76c2bec74e5ba30f0f5861cedb45
AvalonTM <AvalonTM@DESKTOP-2J67J2O>
2025-05-26 14:18:07 -0700

    fix

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 const config = {
   title: 'long play record store',
   descripcion: 'Tu tienda en línea con los mejores productos y ofertas.',
-  apiBase: 'http://66.179.189.108:8081', // Asegúrate de que la URL base esté bien configurada
+  apiBase: 'http://66.179.189.108', // Asegúrate de que la URL base esté bien configurada
   apiUrl: 'http://66.179.189.108:8081/api', // Asegúrate de que la URL base esté bien configurada
-  socketUrl: 'ws://66.179.189.108',
+  socketUrl: 'wss://66.179.189.108',
 };
 export default config;
